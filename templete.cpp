ca3722ed4ceebbe04b59ea0f1a8b6a8efac8f025
oji53 <kanata770311@gmail.com>
2022-11-21 11:46:15 +0900

    update

--- templete.cpp
+++ templete.cpp
@@ -6,10 +6,14 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
 #define all(x) (x).begin(),(x).end()
 #define yes "Yes"
 #define no "No"
 using ll=long long;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
 int main(){
 
